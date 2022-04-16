1e8985ce07db6add9878ec3989227a2d0d09bd32
formazione <gatto.gio@gmail.com>
2022-04-16 18:28:31 +0200

    new commit

--- dictator.py
+++ dictator.py
@@ -9,32 +9,19 @@ from io import BytesIO
 
 
 
-
 pygame.init()
 pygame.mixer.init()
 gameDisplay = pygame.display.set_mode((800, 400))
 
-pngs = [x for x in glob("animals\\*.PNG")]
-names = [x.split(".")[0] for x in glob("animals\\*.PNG")]
-
-animals = {k:v for k, v in zip(pngs, names)}
-print(animals)
 
 def speak(text, language='en'):
+    ''' convert text to speech via google api without saving the object '''
     mp3_fo = BytesIO()
     tts = gTTS(text, lang=language)
     tts.write_to_fp(mp3_fo)
     pygame.mixer.music.load(mp3_fo, 'mp3')
     pygame.mixer.music.play()
 
-
-
-print(pngs)
-print(names)
-keys = list(animals.keys())
-shuffle(keys)
-
-score = 0
 rec_text = ""
 loop = 1
 while loop:
@@ -44,15 +31,13 @@ while loop:
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        # speak("speak")
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
             print(text)
             rec_text += text
         except:
-            print('Did not get that try Again')
-            speak("No, it's wrong")
+            speak("Repeat")
             text=''
         if text == "stop":
             loop = 0
